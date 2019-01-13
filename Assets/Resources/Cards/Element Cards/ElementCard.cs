c31cda55ccd70c72ca4bafc30c4ad3ea7728148e
Karl So <karlsohelloworld@hotmail.com>
2019-01-13 21:44:01 +0800

    New Features@2019-1-13
    +Element Card Merge Function

---
+++ Assets/Resources/Cards/Element Cards/ElementCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Element Card",menuName ="Cards/Element Card")]
+public class ElementCard : ScriptableObject {
+
+    
+    //card name
+    public new string name;
+    //card description
+    public string description;
+    //card image
+    public Sprite cardImage;
+
+
+    //debug purpose,show the information of the card
+    public void print()
+    {
+        Debug.Log("name:" + name + "/description:" + description);
+    }
+
+}
